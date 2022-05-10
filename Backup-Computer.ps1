2d35f79de30d78bca4a19d271ad9f5b5b3699d16
zac-cash <Zachary.cash@rate.com>
2022-05-10 13:03:25 -0500

    first commit

---
+++ Backup-Computer.ps1
@@ -0,0 +1,159 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+$log = [PSCustomObject]@{
+    Warnings = New-Object System.Collections.ArrayList
+    Errors = New-Object System.Collections.ArrayList
+    runtime = (get-date -Format "yyyy.MM.dd HH:mm:ss")
+    backupLocation = ""
+    Choices = [System.Collections.Generic.List[object]]::new()
+    Passwords = New-Object System.Collections.ArrayList
+}
+$outputDate = (get-date -Format "yyyy.MM.dd")
+
+if (Test-Path $env:OneDriveCommercial) {
+    $backupLocation = ($env:OneDriveCommercial) + "\Laptop Backup " + ($outputDate)
+    $log.backupLocation = $backupLocation
+    Write-Host "Saving to " $backupLocation -BackgroundColor Green
+    $null = $log.Warnings.Add("Saving to OneDrive, please ensure user's OneDrive is syncing.")
+    
+}
+else {
+    New-Item -Path "c:\Laptop Backups" -Force
+    $backupLocation = "c:\Laptop Backups\Laptop Backup " + ($outputDate)
+    New-item -Path $backupLocation -Force   
+
+    $null = $log.Warnings.add("Note unable to save to OneDrive Location. Saving to c:\Laptop Backups")
+    Write-host "Note unable to save to OneDrive Location. Saving to c:\Laptop Backups" -BackgroundColor Yellow
+
+    $log.backupLocation = $backupLocation
+}
+
+"Chrome","Firefox","Edge","Wifi","Printers"| ForEach-Object {
+    $choice = New-Variable -Name "Bool$_" -Value (Read-Host "Would you like to back up $_ ?") -Force -PassThru
+    if ($choice.Value -eq "y"){$choice.Value = $true}
+    else {$choice.Value = $false}
+    $null = $log.Choices.add([PSCustomObject]@{
+        $choice.Name = $choice.Value
+    })
+}
+
+Write-Host "`n=======================================`n" -ForegroundColor Black
+Write-Host "Backing up Microsoft AppData" -BackgroundColor Green
+$null = New-Item "$backupLocation\Microsoft" -ItemType Directory
+"Signatures","Speech","Stationery","Sticky Notes","Templates" | ForEach-Object {
+    if (Test-Path $env:APPDATA\Microsoft\$_){
+        $null = New-Item $backupLocation\Microsoft\$_ -Force -ItemType Directory 
+        Write-Host "Backing up $_" -BackgroundColor Cyan
+        Copy-Item $env:APPDATA\Microsoft\$_\* -Destination $backupLocation\Microsoft\$_ -Force
+    }
+}
+
+#Optional Choices
+Write-Host "`n=======================================`n" -ForegroundColor Black
+Write-Host "Backing up Optional data selected:" -BackgroundColor Green
+
+if ($log.Choices.BoolChrome -eq $true) {
+    write-host "Backing up Chrome Bookmarks" -BackgroundColor Cyan
+
+    $null = New-Item "$backupLocation\Google Chrome" -ItemType Directory
+    Write-Host "Backing up default chrome bookmarks. If user has multiple profiles, be sure to export those." -ForegroundColor Yellow
+    Copy-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Bookmarks" -Destination "$backupLocation\Google Chrome"
+
+    $null = $log.Warnings.Add("Be sure to backup Google saved passwords if not synced to a google profile.")
+}
+
+if ($log.Choices.BoolFirefox -eq $true){
+    write-host "Backing up Firefox Bookmarks" -BackgroundColor Cyan
+
+    $null = New-Item "$backupLocation\FireFox" -ItemType Directory
+    $FirefoxDefault = (get-item "$env:APPDATA\Mozilla\Firefox\Profiles\*.default").FullName
+    Copy-Item "$FirefoxDefault\places.sqlite" -Destination "$backupLocation\Firefox"
+    Copy-Item "$FirefoxDefault\Bookmarkbackups\" -Destination "$backupLocation\Firefox" -Recurse
+
+}
+
+if ($log.Choices.BoolEdge -eq $true){
+    write-host "Backing up Edge Bookmarks" -BackgroundColor Cyan
+
+    $null = New-Item "$backupLocation\Edge" -ItemType Directory
+    $EdgeDefault = "$env:LOCALAPPDATA\microsoft\edge\user data\default"
+    Copy-Item "$EdgeDefault\Bookmarks" -Destination "$backupLocation\Edge"
+
+    $null = $log.Warnings.Add("Be sure to backup Edge saved passwords.")
+}
+
+if ($log.Choices.BoolWifi -eq $true){
+    Write-Host "Backing up Wifi Profiles" -BackgroundColor Cyan
+
+    $null = New-Item "$backupLocation\Wifi Profiles" -ItemType Directory
+    $null = netsh wlan export profile key=clear folder="$backupLocation\Wifi Profiles"
+    $wifiProfileNames = (netsh wlan show profiles) | Select-String "\:(.+)$"
+    $null = New-Item -Path "$backupLocation\Wifi Profiles\ManualRestore.txt" -ItemType File
+    foreach ($profile in $wifiProfileNames) {
+        $name = $profile.Matches.Groups[1].Value.Trim()
+        "netsh wlan add profile filename=`"$backupLocation\Wifi Profiles\Wi-Fi-$name.xml`"" | Add-Content "$backupLocation\Wifi Profiles\ManualRestore.txt" 
+    }
+}
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+if ($log.Choices.BoolChrome -eq $true) {
+    $ChromePass = Read-host "Would you like to back-up Chrome passwords? y/n"
+    if ($ChromePass -eq "y") {
+        Write-host "Please wait for chrome to finish loading. chrome://settings/passwords"
+        Start-process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList  'google.com --profile-directory="Default"' -PassThru
+        Start-Sleep -Seconds 1.5
+        [System.Windows.Forms.SendKeys]::SendWait("^l")
+        Start-Sleep -Milliseconds 200
+        [System.Windows.Forms.SendKeys]::SendWait("chrome://settings/passwords")
+        [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+
+        $null = $log.Passwords.add("Chrome")
+        Pause
+    }  
+}
+if ($log.Choices.BoolEdge -eq $true) {
+    $EdgePass = Read-host "Would you like to back-up Edge passwords? y/n"
+    if ($EdgePass -eq "y") {
+        Write-host "Please wait for Edge to finish loading: edge://settings/passwords"
+        Start-process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList  "bing.com" -PassThru
+        Start-Sleep -Seconds 1.5
+        [System.Windows.Forms.SendKeys]::SendWait("^l")
+        Start-Sleep -Milliseconds 200
+        [System.Windows.Forms.SendKeys]::SendWait("edge://settings/passwords")
+        [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+
+        $null = $log.Passwords.add("Edge")
+        Pause
+    }
+    
+}
+
+if ($log.Choices.BoolPrinters -eq $true){
+    #printbrm.exe does not handle spaces in file paths correctly. creating backup in profile, then copying to backup folder.
+    $null = New-Item "$backupLocation\PrinterExport" -ItemType Directory -Force
+    $null = New-Item "$env:USERPROFILE\Printerexport" -ItemType Directory -Force
+    $printerBackupName = "printers_$(get-date -Format "yyyy.MM.dd-HH.mm.ss").printerExport"
+
+    Write-Host "Working on backing up printers." -BackgroundColor Cyan
+    write-host "This can take a some time depending on how many printers and drivers are installed." -ForegroundColor Yellow
+    Write-host "If you would like to delete old or unused printers. Please do so now via PrintManagement.msc before hitting enter." -ForegroundColor Yellow
+    Pause
+    Write-Host "Starting to back up printers" -ForegroundColor Green
+    [void](C:\Windows\System32\Spool\Tools\printbrm.exe -B -f "$env:USERPROFILE\printerexport\$($printerBackupName)")
+    "To restore, use printbrmui.exe located in: C:\Windows\System32\PrintBrmUi.exe" | Set-Content $backupLocation\PrinterExport\ReadMe.txt
+    Copy-Item "$env:USERPROFILE\printerexport\$($printerBackupName)" -Destination "$backupLocation\PrinterExport" -Force
+
+    $log | Add-Member -MemberType NoteProperty -Name "printerFile" -Value $printerBackupName
+}
+
+Write-Host "=======================================" -ForegroundColor Black
+Write-host "Warnings:" -ForegroundColor Yellow
+$log.Warnings | ForEach-Object {write-host -ForegroundColor Yellow $_}
+
+Write-Host "=======================================" -ForegroundColor Black
+Write-Host "Errors:" -ForegroundColor Red
+$log.Errors | ForEach-Object {write-host -BackgroundColor Red $_}
+Write-Host "=======================================" -ForegroundColor Black
+
+$log | ConvertTo-Json -Depth 4| Set-Content $backupLocation\backup.json
+invoke-item $backupLocation
+pause
---
+++ Restore-backup.ps1
@@ -0,0 +1,123 @@
+[void][System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
+Add-Type -AssemblyName System.Windows.Forms
+cls
+Write-host "This Script will overwrite current files with those in the backup folder.`nOverwritten files will not be saved or recoverable. Would you like to continue?" -ForegroundColor DarkYellow
+do {$exit = read-host "y/n"} while (($exit -ne "y") -and ($exit -ne  "n"))
+if ($exit -eq "n"){exit}
+
+Write-Host "Please select the backup.json in onedrive, created by the backup script." -ForegroundColor Green
+Pause
+
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('UserProfile') }
+$null = $FileBrowser.ShowDialog()
+
+$backupjson = Get-Content -Path ($FileBrowser.FileName) | ConvertFrom-Json
+if (Test-Path $backupjson.backupLocation){
+    Set-Location $backupjson.backupLocation
+    $backupLocation = $backupjson.backupLocation
+}else{
+    Write-host "Unable to find backup location listed in JSON file." -ForegroundColor Red
+    $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
+        RootFolder            = "UserProfile"
+        Description           = "$Env:ComputerName - Select a folder"
+    }
+    $null = $FolderBrowser.ShowDialog()
+    if ($FolderBrowser.SelectedPath -eq ""){
+        Write-host "Location not found. Exiting" -ForegroundColor Red
+        exit
+    }
+}
+
+Write-Host "Attempting to restore Appdata" -ForegroundColor Cyan
+if (test-path $backupLocation\Microsoft\){
+    try {
+        Copy-Item $backupLocation\Microsoft\* -Destination $env:APPDATA\Microsoft\ -Recurse -Force -ErrorAction Stop
+    } catch {
+        Write-host "unable to copy all files due to Office365 apps being open. Please close all but OneDrive and hit enter." -BackgroundColor Red
+        Pause
+        Copy-Item $backupLocation\Microsoft\* -Destination $env:APPDATA\Microsoft\ -Recurse -Force
+    }
+}
+
+$ChromeDefault = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\"
+$ChromeInstalled = test-path $ChromeDefault
+if ($backupjson.Choices.BoolChrome -eq $true -and $ChromeInstalled -eq $true) {
+    Write-Host "Restoring Chrome." -ForegroundColor Cyan
+    Copy-Item "$backupLocation\Google Chrome\Bookmarks" -Destination  $ChromeDefault -Force
+}
+
+$FirefoxDefault = (get-item "$env:APPDATA\Mozilla\Firefox\Profiles\*.default" -ErrorAction SilentlyContinue).FullName
+$firefoxInstalled = test-path "$env:APPDATA\Mozilla\Firefox\Profiles\" -ErrorAction SilentlyContinue
+if (!$firefoxInstalled){
+    Write-Host "Firefox does not appear to be installed." -ForegroundColor Cyan
+}
+if ($backupjson.Choices.BoolFirefox -eq $true -and $firefoxInstalled -eq $true) {
+    Write-Host "Restoring Firefox"
+    Copy-Item "$backupLocation\FireFox\places.sqlite" -Destination "$FirefoxDefault"
+    Copy-Item "$backupLocation\FireFox\Bookmarkbackups\*" -Destination "$FirefoxDefault\Bookmarkbackups\" -Recurse -Force
+}
+
+if ($backupjson.Choices.BoolEdge -eq $true){
+    write-host "Restoring Edge" -ForegroundColor Cyan
+    Copy-Item "$backupLocation\Edge\Bookmarks" -Destination "$env:LOCALAPPDATA\microsoft\edge\user data\default"
+}
+
+if ($backupjson.Choices.BoolWifi -eq $true) {
+    Write-Host "Restoring Wifi" -ForegroundColor Cyan
+    $wifiDecision = write-host "Would you like to delete the wifi profiles after importing? They contain clear text passwords." -ForegroundColor Yellow
+    Get-ChildItem -Path "$backupLocation\Wifi Profiles" -Exclude "*.txt" | ForEach-Object {netsh wlan add profile filename="$_"}
+
+    if ($wifiDecision -eq "y") {
+        Get-ChildItem -Path "$backupLocation\Wifi Profiles" -Exclude "*.txt" | Remove-Item -Force
+    }
+}
+
+if ($backupjson.Choices.BoolPrinters -eq $true) {
+    Write-Host "Restoring Printers. Please enter administrator credentials for printer installation."
+    if (test-path $backupLocation\PrinterExport\*.printerExport){
+        $PrinterFile = (get-item $backupLocation\PrinterExport\*.printerExport)[0].Name
+        New-item -ItemType Directory "$env:USERPROFILE\printerRestore" -ErrorAction SilentlyContinue
+        Copy-Item -Path "$backuplocation\PrinterExport\$Printerfile" -Destination "$env:USERPROFILE\printerRestore\"
+
+        $path = "$env:USERPROFILE\printerRestore\$Printerfile"
+        Start-Process cmd -ArgumentList "/k C:\Windows\System32\spool\tools\PrintBrm.exe -r -f $path" -Credential (Get-Credential)
+    }
+}
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
+if ($backupjson.Choices.BoolChrome -eq $true) {
+    $ChromePass = Read-host "Would you like to restore Chrome passwords? Selecting yes will attempt to open Chrome with the import flag checked. y/n"
+    if ($ChromePass -eq "y") {
+        Write-host "Please wait for chrome to finish loading. chrome://settings/passwords"
+        try{
+            Start-process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -ArgumentList  'google.com --profile-directory="Default" -enable-features=PasswordImport'
+        }catch{
+            write-host "unable to find in x86"
+            Start-process "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList  'google.com --profile-directory="Default" -enable-features=PasswordImport'
+        }
+        Start-Sleep -Seconds 2
+        [System.Windows.Forms.SendKeys]::SendWait("^l")
+        Start-Sleep -Milliseconds 200
+        [System.Windows.Forms.SendKeys]::SendWait("chrome://settings/passwords")
+        [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+
+        Pause
+    }  
+}
+
+if ($backupjson.Choices.BoolEdge -eq $true) {
+    $EdgePass = Read-host "Would you like to restore Edge passwords? This will attempt to open Edge directly to the passwords screen. y/n"
+    if ($EdgePass -eq "y") {
+        Write-host "Please wait for Edge to finish loading: edge://settings/passwords"
+        Start-process "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -ArgumentList  "bing.com"
+
+        Start-Sleep -Seconds 2
+        [System.Windows.Forms.SendKeys]::SendWait("^l")
+        Start-Sleep -Milliseconds 200
+        [System.Windows.Forms.SendKeys]::SendWait("edge://settings/passwords")
+        [System.Windows.Forms.SendKeys]::SendWait("{Enter}")
+    }
+
+    Write-Host "finished" -ForegroundColor Green
+    Pause
+}
